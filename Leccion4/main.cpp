7e331fdb22e8f56c97cc8657205d294d3bcc6899
MatrixMYL2021 <ww.arg.realmadrid.an@gmail.com>
2024-05-29 15:16:23 -0300

    Leccion 6 terminada, addtoker token count, falta ver templates y tad coll leccion 7 y empezar con la 8

--- Leccion4/main.cpp
+++ Leccion4/main.cpp
@@ -3,10 +3,28 @@
 using namespace std;
 
 int main() {
-  string eleccion="Viva la libertad carajo";
-  int d=8; int h=15;
-  cout<<"Extracto: "<<substring(eleccion,d,h)<<endl;
-  cout<<"cant char: "<<charCount(eleccion,'c')<<endl;
-  cout<<"contador de caract: "<<length(eleccion)<<endl;
+  string lema="Viva la libertad caraja";
+
+  cout<<"1.1.1.1 : "<<"length= "<<length(lema)<<endl;
+  cout<<"1.1.1.2 : "<<"Veces que aparece el char = "<<charCount(lema,'l')<<endl;
+  cout<<"1.1.1.3 : "<<substring(lema,8,15)<<endl;
+  cout<<"1.1.1.4 : "<<overString(lema,5)<<endl;
+  cout<<"1.1.1.5 : "<<"Posicion = "<<indexOf(lema,'x')<<endl;
+  cout<<"1.1.1.6 : "<<"Posicion = "<<indexOf2(lema,'i',1)<<endl;
+  cout<<"1.1.1.7 : "<<"Posicion = "<<indexOf3(lema, "l")<<endl;
+
+  string s="";
+  int n =tokenCount(s,'|');
+  cout<<"Cant de tokens: "<<n<<endl;
+
+  string a="";
+  string c="Martinez";
+  char b='|';
+  string lol=addToken(a,b,c);
+  //string lol2=a+b+c;
+  std::cout << lol << '\n';
+
+
+
 
 }
